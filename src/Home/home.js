226146521c0a58355596d0e7243a0138f22c9595
alexdavila90 <alexdavila90@hotmail.com>
2025-03-20 19:57:16 -0500

    Actualizacion del Home

--- src/Home/home.js
+++ src/Home/home.js
@@ -77,7 +77,24 @@ nextBtn.addEventListener("click", () => {
 // Renderiza el primer flat
 renderFlat(currentIndex);
 
-
+document.addEventListener("DOMContentLoaded", function () {
+    const counters = document.querySelectorAll('.counter');
+    counters.forEach(counter => {
+        counter.innerText = '0';
+        const updateCounter = () => {
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText;
+            const increment = target / 100;
+            if (count < target) {
+                counter.innerText = `${Math.ceil(count + increment)}`;
+                setTimeout(updateCounter, 20);
+            } else {
+                counter.innerText = target;
+            }
+        };
+        updateCounter();
+    });
+});
 
 
 
